4ba7ce73b49e3331ac475c4c53494cb695423f5b
Lena <urbangirl@mail.ru>
2018-08-18 14:37:36 +0300

    using preconditions

--- addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.island.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.island.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereaGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returntoGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/GroupModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/GroupModificationsTests.java
@@ -7,9 +7,12 @@ public class GroupModificationsTests extends TestBase {
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereaGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returntoGroupPage();
     }
