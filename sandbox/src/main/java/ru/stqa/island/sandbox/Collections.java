1de103d419ee1507cede50bf039f10e0b92c5f52
Lena <urbangirl@mail.ru>
2018-08-19 15:42:10 +0300

    collections

---
+++ sandbox/src/main/java/ru/stqa/island/sandbox/Collections.java
@@ -0,0 +1,17 @@
+package ru.stqa.island.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+    public static void main(String[] args) {
+        String[] langs = {"Java", "C#", "Python", "Php"};
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "Php");
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
+        }
+    }
+}
