7e5618e16c4bfffe80b5e87fdf2b4089e9871914
Lena <urbangirl@mail.ru>
2018-09-04 00:36:30 +0300

    #12 + #13

--- addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/model/ContactData.java
@@ -1,13 +1,23 @@
 package ru.stqa.island.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
 public class ContactData {
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String firstname;
+    @Expose
     private String lastname;
     private String nickname;
+    @Expose
     private String address;
+    @Expose
     private String homePhone;
     private String mobilePhone;
     private String workPhone;
@@ -165,14 +175,6 @@ public class ContactData {
     }
 
     @Override
-    public String toString() {
-        return "ContactData{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -190,5 +192,13 @@ public class ContactData {
         result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
         result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/tests/ContactCreationTests.java
@@ -1,41 +1,49 @@
 package ru.stqa.island.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.island.addressbook.model.ContactData;
 import ru.stqa.island.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testNewContact() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test (dataProvider = "validContactsFromJson")
+    public void testNewContact(ContactData contact) {
         app.goTo().returntoHomePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/succulentcupcakes.jpg");
-        ContactData contact = new ContactData().withFirstname("Bruce").withLastname("Wayne").withNickname("Batman")
-                .withAddress("Gotham City, Freedom str, 1").withHomePhone("").withEmail("brucewayne123@gmail.com")
-                .withYearofbirth("1982").withGroup("test1")
-                .withPhoto(photo);
         app.contact().create(contact, true);
         app.goTo().returntoHomePage();
         Contacts after = app.contact().all();
-        //assertThat(app.contact().count(), equalTo(before.size() + 1));
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(before.withAdded(
                 contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-    }
-
-    @Test (enabled = false)
-    public void testCurrentDir() {
-        File currentDir = new File(".");
-        System.out.println(currentDir.getAbsolutePath());
-        File photo = new File("src/test/resources/succulentcupcakes.jpg");
-        System.out.println(photo.getAbsolutePath());
-        System.out.println(photo.exists());
     }
 
     @Test (enabled = false)
