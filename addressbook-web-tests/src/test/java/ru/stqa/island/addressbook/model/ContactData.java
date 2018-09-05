e70a1490c55397ff7a7fa233dd4066124ebd299f
Lena <urbangirl@mail.ru>
2018-09-05 23:56:03 +0300

    7.3 contactdata connection test

--- addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/island/addressbook/model/ContactData.java
@@ -3,39 +3,63 @@ package ru.stqa.island.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table (name = "addressbook")
 @XStreamAlias("contact")
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column (name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column (name = "firstname")
     private String firstname;
     @Expose
+    @Column (name = "lastname")
     private String lastname;
     private String nickname;
     @Expose
+    @Type (type = "text")
     private String address;
     @Expose
+    @Column (name = "home")
+    @Type(type = "text")
     private String homePhone;
+    @Column (name = "mobile")
+    @Type (type = "text")
     private String mobilePhone;
+    @Column (name = "work")
+    @Type (type = "text")
     private String workPhone;
+    @Transient
     private String allPhones;
+    @Transient
     private String email;
+    @Transient
     private String email2;
+    @Transient
     private String email3;
+    @Transient
     private String allEmails;
+    @Transient
     private String yearofbirth;
+    @Transient
     private String group;
-    private File photo;
+    @Column (name = "photo")
+    @Type (type = "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
